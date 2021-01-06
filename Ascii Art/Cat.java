6148feb2c48e247fe070648b8aabf1cbc39f4ea7
Minho Cho <mc811mc@gmail.com>
2021-01-06 21:05:07 +0900

    Add Ascii Art

---
+++ Ascii Art/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+   public static void main(String[] args) {
+      cat();
+   }
+   
+   public static void cat() {
+        System.out.println("^__^");
+        System.out.println("(");
+        System.out.println("");
+        System.out.println("| __ |");
+        System.out.println("(__)");
+        System.out.println();
+    }
+}
